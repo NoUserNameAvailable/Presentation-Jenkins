dcc2fe625325161b38396ee5893d2792c32571a1
Mathieu <mathieu.laplaige@gmail.com>
2016-10-12 17:17:46 +0200

    Initial commit

---
+++ src/main/java/RestApplication.java
@@ -0,0 +1,10 @@
+/**
+ * Created by mat on 12/10/2016.
+ */
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestApplication extends Application {
+
+}
